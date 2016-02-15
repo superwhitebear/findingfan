10191060c06e5228d6baa4ab9aecde991789424e
jettamon <themai@TheMais-MacBook-Air.local>
2016-02-15 20:53:10 +0700

    List All profile from Collection profiles

--- routes/list.js
+++ routes/list.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next){
 
 	if (req.session.username){
 		//res.send(req.session.username);
+		var db = req.db;
+    	var collection = db.get('profiles');
 		collection.find({},{},function(e,docs){
     		res.render('list', docs);	
   		})
