e0cc62e70f6c3fa0686f06d3b4a930cf0c57e981
phynya <amy_air@hotmail.com>
2016-02-15 15:26:37 +0700

    index กับ login jade by amy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Log In' });
 });
+
+router.get('/list', function(req, res, next) {
+  res.render('list', { title: 'List of FANs' });
+});
+
 
 module.exports = router;
