a6aa788c59abf97708bcb6f5d722539b991f8aa5
jettamon <themai@TheMais-MacBook-Air.local>
2016-02-15 22:30:48 +0700

    Add register page

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Log In' });
 });
 
+router.get('/regis',function(req, res, next) {
+	res.render('register.jade',{ title: 'Register'});
+	//res.send('Register Form');
+});
+
+router.post('/createaccount',function(req, res, netxt){
+
+    if (req.body.username!=''&&req.body.password!=''&&req.body.password==req.body.confirmpassword){
+    	var db = req.db;
+		var collection = db.get('users');
+
+	    collection.insert({username: req.body.username,passwd: req.body.password}, function(err, docs) {
+	        if (err) {
+	            res.send('Cannot Create Account <META http-equiv="refresh" content="1;URL=http://localhost:3000">');
+	        } else {
+	            // redirect to /users 
+	            res.send('Account Created <META http-equiv="refresh" content="1;URL=http://localhost:3000">');
+	        }
+	    })
+	}else {
+		res.send('Data Input Error!! Please, Try again <META http-equiv="refresh" content="1;URL=http://localhost:3000/regis">');
+	}
+    
+});
+
 
 module.exports = router;
