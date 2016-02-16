0ae5d96e502d0cae26e1c52b2fd7b2b22f64e645
jettamon <jet_tamon@hotmail.com>
2016-02-16 15:22:35 +0700

    Add Authen check

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  	var usrname = req.body.username;
+	var	passwd = req.body.password;
+	
+	if (usrname == 'admin' && passwd =='1234'){
+
+		req.session.usrname = usrname;
+		res.redirect('/list')
+	}else{
+		
+		res.redirect('/');
+	}
 });
 
 module.exports = router;
