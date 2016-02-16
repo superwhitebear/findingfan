ad05956f66eff2ab7bb230c89fee73b0146a33c4
jettamon <jet_tamon@hotmail.com>
2016-02-16 17:27:07 +0700

    check session

---
+++ routes/list.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next){
+
+	if (req.session.usrname){
+		//res.send(req.session.usrname);
+		res.render('list', { username: req.session.usrname });
+	}else{
+		res.redirect('/');
+	}
+});
+  
+
+router.get('/profile',function(req, res, next){
+	res.render('profile');
+});
+
+router.get('/search/',function(req, res ,next){
+	res.render('search', {username: req.params.usrname});
+});
+
+router.post('/search/:usrname',function(req, res, next){
+
+});
+module.exports = router;
